fixed bugs in backprop

server/model/Layers/connected.py
@@ -1,5 +1,5 @@
 import numpy as np
-from activations import softmax
+from utils import softmax
 
 
 class ConnectedLayer:
@@ -10,9 +10,18 @@ class ConnectedLayer:
     - soft: whether the activation is softmax, bool
     - cache: cache to hold input and output of a ff step that is used in backprop, dict
     - retain_chance: chance that a neuron will not be disactivated during dropout, -1 if not dropout, float
+    - trainable: whether or not this layer can be trained, bool
     """
 
+    trainable = True
+
     def __init__(self, input, output, activation=softmax, dropout=False):
+        """parameters -
+        - input: dimensions of input into layer, int
+        - output: dimensions of output from layer, int
+        - activation: activation fn, function
+        - dropout: whether or not to implement dropout, bool if not, float representing percent of neurons to deactivate
+        if so"""
         self.w = np.random.rand(output, input + 1)
         self.activation = activation()
         self.cache = {'in': None, 'z': None, 'a': None}
@@ -53,6 +62,7 @@ class ConnectedLayer:
             self.cache['in'] = np.copy(X)
             self.cache['z'] = np.copy(z)
             self.cache['a'] = np.copy(a)
+        print('\nConnectedLayer\ninput:', X.shape,'\noutput:', a.shape)
         return a
 
     def backprop(self, dE_da):
@@ -71,8 +81,12 @@ class ConnectedLayer:
             dz = dE_da * self.activation.deriv(self.cache['z'], deriv=True)
         dw = dz.dot(np.vstack([self.cache['in'], np.ones((1, 1))]).T)
         dE_dIn = self.w[:, :-1].T.dot(dz)
+        print('\nConnectedLayer backprop:\nInput:', dE_da.shape, '\ngradient:', dw.shape)
         return dE_dIn, dw
 
     def update(self, dw):
-        """updates self.w, assumes that dw has already been multiplied by a learning rate and any other necessary constants."""
+        """updates self.w, assumes that dw has already been multiplied by a learning rate and any other necessary
+        constants.
+        parameters -
+        - dw: np.ndarray"""
         self.w -= dw

server/model/Layers/conv.py
@@ -1,5 +1,5 @@
 import numpy as np
-from model.activations import relu
+from utils import relu
 
 class ConvLayer:
     """Convolutional layer of a CNN
@@ -10,7 +10,10 @@ class ConvLayer:
     - activation: activation function to be applied to output, function
     - cache: information on forward pass used in backprop, dict
     - conv_dim: dimension of one side of the output, int
+    - trainable: whether or not this layer can be trained, bool
     """
+
+    trainable = True
 
     def __init__(self, filters, mode='max', activation=relu):
         if len(filters) == 3:
@@ -59,7 +62,7 @@ class ConvLayer:
             self.cache['in'] = X.astype(np.float64)
             self.cache['z'] = conv_features
             self.cache['a'] = a
-
+        print('\nConvLayer\ninput:', X.shape,'\noutput:', a.shape)
         return a
 
     def backprop(self, dE_da):
@@ -90,7 +93,12 @@ class ConvLayer:
         # remove padding if necessary
         if self.mode == 'max':
             dE_dIn = dE_dIn[:, fshape[-1]-1:-fshape[-1]-1, fshape[-1]-1:fshape[-1]-1]
+        print('\nConnectedLayer backprop:\nInput:', dE_da.shape, '\ngradient filter:', dw.shape, '\ngradient bias:', db.shape)
         return dE_dIn, dw, db
+
+    def update(self, gf, gb):
+        self.filters -= gf
+        self.bias -= gb
 
 
 #### HELPER FUNCTIONS ####