dee585376b97dc149163ff7677459d5e7ba4888a
jacobhanson <jhanreg11@gmail.com>
2019-11-03 21:56:21 -0800

    working on vectorizing conv layer

--- server/model/layers/conv.py
+++ server/model/layers/conv.py
@@ -1,6 +1,7 @@
 import numpy as np
 from utils import relu
 
+
 class ConvLayer:
     """Convolutional layer of a CNN
     Data members -
@@ -15,13 +16,14 @@ class ConvLayer:
 
     trainable = True
 
-    def __init__(self, filters, mode='max', activation=relu):
+    def __init__(self, filters, mode='max', activation=relu, first_layer=False):
         if len(filters) == 3:
             filters = (filters[0], 1, filters[1], filters[2])
         self.filters = np.random.rand(filters[0], filters[1], filters[2], filters[3])
-        self.bias = np.random.rand(filters[0])
+        self.bias = np.random.rand(filters[0], 1) # created as column matrix for fully vectorized training
         self.mode = mode
         self.activation = activation()
+        self.first_layer = first_layer
         self.cache = {'in': None, 'z': None, 'a': None}
 
     def ff(self, X, cache=False):
@@ -52,7 +54,7 @@ class ConvLayer:
         for i in range(filter_number):
             conv_image = np.zeros((self.conv_dim, self.conv_dim))
             for j in range(image_channels):
-                conv_image += convolve(X[j], self.filters[i, j], self.mode) + self.bias[i]
+                conv_image += fftconvolve(X[j], np.rot90(self.filters[i, j], 2), self.mode) + self.bias[i]
             conv_features[i] = conv_image
 
         a = self.activation.reg(conv_features)
@@ -64,6 +66,55 @@ class ConvLayer:
             self.cache['a'] = a
         # print('\nConvLayer\ninput:', X.shape,'\noutput:', a.shape)
         return a
+
+    def vectorized_ff(self, X, cache=False):
+        """Fully vectorized feedforward for a batch of inputs
+        parameters -
+        - X: tensor of all input images, 4d np.ndarray num_images X image_channels X image_dim X image_dim
+        - cache: cache, bool
+        return -
+        - a: activation tensor representing output of all samples"""
+        if self.mode == 'max':
+            X = pad(X, self.filters.shape[-1])
+
+        X = self.vectorize_input(X)
+
+        filter_matrix = self.vectorize_filters()
+
+        return filter_matrix.dot(X) + self.bias
+
+    def vectorize_filters(self):
+        filter_dim = self.filters.shape
+        filter_length = filter_dim[1]*filter_dim[2]*filter_dim[3]
+        filter_matrix = np.zeros((filter_dim[0], filter_length))
+        for filter_number in range(filter_dim[0]):
+            filter_matrix[filter_number] += self.filters[filter_number].reshape(filter_length)
+        print('\nfilter matrix:\n', filter_matrix, '\nbiases:\n', self.bias)
+        return filter_matrix
+
+    def vectorize_input(self, X):
+        print('mini batches:\n', X, '\nfilters:\n', self.filters)
+        input_dim = X.shape
+        filter_dim = self.filters.shape
+        self.conv_dim = input_dim[-1] - filter_dim[-1] + 1
+
+        if len(input_dim) == 4:
+            layer_input = np.zeros(
+                (filter_dim[1] * filter_dim[2] * filter_dim[3], input_dim[0] * self.conv_dim ** 2))
+            for image_number in range(input_dim[0]):
+
+                # columns corresponding to this input image
+                col_start = image_number * self.conv_dim ** 2
+                col_end = (image_number + 1) * self.conv_dim ** 2
+
+                for image_channel in range(input_dim[1]):
+                    # rows corresponding to this image channel
+                    row_start = image_channel * filter_dim[2] * filter_dim[3]
+                    row_end = (image_channel + 1) * filter_dim[2] * filter_dim[3]
+                    layer_input[row_start:row_end, col_start:col_end] += im2col_2d(X[image_number, image_channel],
+                                                                                   (filter_dim[2], filter_dim[3]))
+        print('\nlayer input:\n', layer_input)
+        return layer_input
 
     def backprop(self, dE_da):
         """calculates gradient of filters, biases, and inputs into layer
@@ -81,7 +132,7 @@ class ConvLayer:
 
         # add padding if necessary
         if self.mode == 'max':
-            X = pad(X, fshape[-1]-1)
+            X = pad(X, fshape[-1] - 1)
 
         dw = np.zeros_like(self.filters)
         dE_dIn = np.zeros_like(X)
@@ -92,9 +143,14 @@ class ConvLayer:
 
         # remove padding if necessary
         if self.mode == 'max':
-            dE_dIn = dE_dIn[:, fshape[-1]-1:-fshape[-1]-1, fshape[-1]-1:fshape[-1]-1]
+            dE_dIn = dE_dIn[:, fshape[-1] - 1:-fshape[-1] - 1, fshape[-1] - 1:fshape[-1] - 1]
         # print('\nConnectedLayer backprop:\nInput:', dE_da.shape, '\ngradient filter:', dw.shape, '\ngradient bias:', db.shape)
         return dE_dIn, dw, db
+
+    def vectorized_backprop(self, dE_da):
+        """calculates gradients of filters and inputs into layer for a full batch
+        parameters -
+        - dE_da: gradient of error wrt to activated output, """
 
     def update(self, gf, gb):
         self.filters -= gf
@@ -103,6 +159,28 @@ class ConvLayer:
 
 #### HELPER FUNCTIONS ####
 def convolve(image, feature, border='max'):
+
+    if border == 'max':
+        image = pad(image, feature.shape[-1]-1)
+
+    image_dim = np.array(image.shape)
+    feature_dim = np.array(feature.shape)
+
+    target_dim = image_dim - feature_dim + 1
+    if np.any(target_dim < 1):
+        target_dim = feature_dim - image_dim + 1
+    target = np.zeros(target_dim)
+    for row in range(target_dim[0]):
+        start_row = row
+        end_row = row + feature_dim[0]
+        for col in range(target_dim[1]):
+            start_col = col
+            end_col = col + feature_dim[1]
+            target[row,col] = np.sum(image[start_row:end_row, start_col:end_col]*feature)
+
+    return target
+
+def fftconvolve(image, feature, border='max'):
     """2d convolution using fft
     parameters -
     - image: image to be convolved, 2d nxn np.ndarray
@@ -110,6 +188,7 @@ def convolve(image, feature, border='max'):
     - border: border mode, str
     return -
     - target: convolution output, 2d np.ndarray"""
+
     image_dim = np.array(image.shape)
     feature_dim = np.array(feature.shape)
     target_dim = image_dim + feature_dim - 1
@@ -141,3 +220,31 @@ def pad(image, extra_layers):
     assert len(image.shape) == 3, f'invalid input, expected 3d ndarray, given {image.shape}'
     return np.pad(image, ((0, 0), (extra_layers, extra_layers), (extra_layers, extra_layers)), 'constant')
 
+
+def im2col_2d(X, patch_size, stepsize=1):
+    """Matlab's im2col function implemented using broadcasting for efficiency. turns a 2d input image into another 2d
+    matrix where every col is a convolutional patch
+    >>> a = np.arange(16).reshape((4, 4))
+    >>> a
+    array([[ 0,  1,  2,  3],
+           [ 4,  5,  6,  7],
+           [ 8,  9, 10, 11],
+           [12, 13, 14, 15]])
+    >>> im2col_2d(a, (2, 2))
+    array([[ 0,  1,  2,  4,  5,  6,  8,  9, 10],
+           [ 1,  2,  3,  5,  6,  7,  9, 10, 11],
+           [ 4,  5,  6,  8,  9, 10, 12, 13, 14],
+           [ 5,  6,  7,  9, 10, 11, 13, 14, 15]])
+    """
+    rows, cols = X.shape
+    col_extent = cols - patch_size[1] + 1
+    row_extent = rows - patch_size[0] + 1
+
+    # Get Starting block indices
+    start_idx = np.arange(patch_size[0])[:, None] * cols + np.arange(patch_size[1])
+
+    # Get offsetted indices across the height and width of input array
+    offset_idx = np.arange(row_extent)[:, None] * cols + np.arange(col_extent)
+
+    # Get all actual indices & index into input array for final output
+    return np.take(X, start_idx.ravel()[:, None] + offset_idx.ravel()[::stepsize])
--- server/model/tests.py
+++ server/model/tests.py
@@ -4,6 +4,7 @@ from layers.conv import ConvLayer
 from layers.maxpool import MaxPoolLayer
 from layers.flatten import FlattenLayer
 
+
 def dense_test():
     c = ConnectedLayer(2, 2)
     x = np.random.rand(2, 1)
@@ -14,10 +15,12 @@ def dense_test():
     other_dE_dIn = c.w[:, :-1].T.dot(dE_dA)
     print(np.isclose(dw, other_dw), np.isclose(dE_dIn, other_dE_dIn))
 
+
 def dropout_test():
     c = ConnectedLayer(2, 2, dropout=.5)
     c.ff(np.random.rand(2, 1), True)
     print(c.cache['in'], c.cache['a'])
+
 
 def pool_test():
     m = MaxPoolLayer(3)
@@ -27,6 +30,7 @@ def pool_test():
     dE_dIn = m.backprop(dE_dOut)
     print(x, '\n', dE_dIn)
 
+
 def conv_test():
     c = ConvLayer((3, 1, 2, 2), 'valid')
     x = np.arange(16).reshape((1, 4, 4))
@@ -34,7 +38,19 @@ def conv_test():
     dE_dA = np.random.rand(3, 3, 3)
     print([o.shape for o in c.backprop(dE_dA)])
 
+
+def vectorized_conv_test():
+    c = ConvLayer((3, 3, 2, 2), 'valid', first_layer=True)
+    x = np.arange(375).reshape((5, 3, 5, 5))
+    print('vectorized output:\n', c.vectorized_ff(x), '\nnon vectorized output:\n')
+    for i in range(5):
+        print(c.ff(x[i]))
+
+
 def flatten_test():
     f = FlattenLayer()
     f.ff(np.random.rand(3, 5, 5), True)
     print(f.backprop(np.random.rand(75, 1)).shape)
+    
+
+vectorized_conv_test()
