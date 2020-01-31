7d966f785c3b528b894bb59024b66ef93bd95df4
jacobhanson <jhanreg11@gmail.com>
2020-01-31 15:35:50 -0800

    started api

--- server/controllers.py
+++ server/controllers.py
@@ -1,17 +1,12 @@
 from server import app, db
 from flask import request, jsonify
 from datetime import datetime, timedelta
-from server.models.conv_net import *
+from server.model.NN import NN
+from scipy.misc import imread
 
-cnn = CNN({}, 'test')
+cnn = NN.load_weights()
 
-@app.route('/api/classify', methods=['GET'])
-def get_classify():
-    data = request.get_json(force=True)
-    image = data['image']
-    h = cnn.predict(image)
-    return jsonify({'result': h})
-
-@app.route('/api/entry', methods=['POST'])
-def post_entry():
-    pass
+@app.route('/api/classify', methods=['POST'])
+def classify():
+  data = request.data
+  im = imread
